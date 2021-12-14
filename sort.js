a9ddca01df565cb962368707fef1b96296e55d78
trunghieu2426 <phamtrunghieu.dn2426@gmail.com>
2021-12-14 21:53:39 +0700

    pc

---
+++ sort.js
@@ -0,0 +1,33 @@
+function bubbleSort(arr) {
+  let noSwap;
+  for (let i = arr.length; i > 0; i--) {
+    noSwap = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+
+  return arr;
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      [arr[min], arr[i]] = [arr[i], arr[min]];
+    }
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([3, 1, 2, 4]));
